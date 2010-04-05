365d68620d0bcd82f11a0a85593785f24f92122c
Philip (flip) Kromer <flip@infochimps.org>
2010-04-05 06:01:46 -0500

    Making all activity live in the Broham simpleDB domain

--- lib/broham/script.rb
+++ lib/broham/script.rb
@@ -1,21 +1,17 @@
-module Broham
+class Broham < RightAws::ActiveSdb::Base
 
   def self.get_cluster_settings
     Configliere.use :commandline, :config_file
     Settings.read('broham.yaml')
     Settings.resolve!
-    Settings[:cluster_name] = Settings.rest.shift
     Settings[:role_name]    = Settings.rest.shift
     check_args!
-    cluster = Broham.new(Settings[:cluster_name])
-    cluster.establish_connection
-    cluster.create_domain
-    cluster
+    self.establish_connection
   end
 
   def self.check_args!
-    if (Settings[:cluster_name].blank? || Settings[:role_name].blank?)
-      warn "Please supply a cluster name and a role as the first two arguments"
+    if Settings[:role_name].blank?
+      warn "Please supply a role as the first argument"
       exit(-1)
     end
   end
