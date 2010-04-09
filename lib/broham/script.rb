e09d7c9afc2d41d4a2025e1a43d4fe063f9cbe3f
Philip (flip) Kromer <flip@infochimps.org>
2010-04-08 21:23:07 -0500

    tidier scripts

--- lib/broham/script.rb
+++ lib/broham/script.rb
@@ -4,12 +4,17 @@ class Broham < RightAws::ActiveSdb::Base
     Configliere.use :commandline, :config_file
     Settings.read('broham.yaml')
     Settings.resolve!
-    Settings[:role_name]    = Settings.rest.shift
-    check_args!
     self.establish_connection
   end
 
-  def self.check_args!
+  def self.get_command_line_args *arg_names
+    arg_names.each do |arg_name|
+      Settings[arg_name] = Settings.rest.shift
+    end
+    check_args! *arg_names
+  end
+
+  def self.check_args! *arg_names
     if Settings[:role_name].blank?
       warn "Please supply a role as the first argument"
       exit(-1)
